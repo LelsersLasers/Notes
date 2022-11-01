113843ccc58ea1db1c792f9b0ab81da4f967498c
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2022-10-31 18:20:44 -0700

    Day 46: Improving koch vissual

--- Math/548/koch.py
+++ Math/548/koch.py
@@ -1,3 +1,4 @@
+import math
 from turtle import *
 
 
@@ -11,19 +12,12 @@ def koch(size, depth):
 
 
 def main():
-    speed(3)
+    speed(0)
     colormode(255)
     pensize(2)
     bgcolor("black")
 
-    size = 500
-
-    penup()
-    backward(size / 2)
-    left(90)
-    forward(size / 4)
-    right(90)
-    pendown()
+    size = 150
 
     colors = [
         "red",
@@ -33,9 +27,24 @@ def main():
     ]
 
     for n in range(4):
+
         color(colors[n])
+
+
+        penup()
+        
+        s = size * (n + 1)
+        b = s / 2 - (size * n) / 2
+        f = s / math.sqrt(12) - (size * n) / math.sqrt(12)
+
+        backward(b)
+        left(90)
+        forward(f)
+        right(90)
+        pendown()
+        
         for _i in range(3):
-            koch(size, n)
+            koch(s, n)
             right(120)
 
     while True:
