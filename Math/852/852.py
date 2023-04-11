b0ec8c63d1eca386f25645a1dea3bbde9c6fcad1
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-04-10 19:04:31 -0700

    Day 129: Math: problem 852 as imperative recursion

---
+++ Math/852/852.py
@@ -0,0 +1,10 @@
+
+def a_n(last: float, amount: float) -> float:
+    return last * 1.01 - amount
+
+monthly_options = [150, 200, 250, 300, 350, 263.338]
+for monthly in monthly_options:
+    a = 10_000
+    for _i in range(48):
+        a = a_n(a, monthly)
+    print("%f: %f" % (monthly, a))
